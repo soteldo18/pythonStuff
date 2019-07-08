70318cf8428e7475c9c800d98e2a89ed0fb547fe
Enrique Soteldo <soteldo18@gmail.com>
2019-07-08 11:53:18 -0400

    modified:   serializacion.py

--- serializacion.py
+++ serializacion.py
@@ -9,15 +9,19 @@ except ImportError:
 
 
 animales= ["piton","mono","camello"]
+lenguajes=["python","java","perl"]
 
 with open("data.pickle","wb") as fichero:
 	pickle.dump(animales,fichero,2)
-	
+	pickle.dump(lenguajes,fichero,2)
 	fichero.close()
 
 
 with open("data.pickle","rb") as fichero:
 	animales2=pickle.load(fichero)
+	lenguajes2=pickle.load(fichero)
+	
 
 print(animales2)
+print(lenguajes2)
 
